35662a78c230e49639e4cae647ad3b5eb05e37ac
priyanka nigam <priyanka.nigam_cs18@gla.ac.in>
2020-11-17 02:52:50 +0530

    added images

---
+++ Restaurant Front-end/javascript/main.js
@@ -0,0 +1,11 @@
+// const food = document.getElementById("food");
+
+// function showtime() {
+//   let today = new Date();
+//   let todaydate = today.toDateString();
+
+//   food.innerHTML = `${todaydate}`;
+//   setTimeout(showtime, 1000);
+// }
+
+// showtime();
